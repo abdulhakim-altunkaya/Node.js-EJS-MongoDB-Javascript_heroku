8303efd45271fe2adfea9eb2e931ae93c0ba15b0
abdulhakim_altunkaya <altunkaya2@gmail.com>
2021-05-30 19:03:41 +0300

    Commit 2: another page added

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const app = express();
 app.get("/", function(req, res){
   res.send("it is  working dude");
 });
+app.get("/home", function(req, res){
+  res.send("It is really working dude");
+});
 
 app.listen(process.env.PORT || 5000);
