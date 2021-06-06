31e3df3525c2f7da3d6ca2a23972bd39123e7167
abdulhakim_altunkaya <altunkaya2@gmail.com>
2021-06-06 21:35:30 +0300

    Commit 10: index.js modified for error

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
+
 app.get("/test", function(req, res){
-  res.sendFile('/test.html');
+  res.sendFile(path.join(__dirname, '/test.html'));
 });
 app.get("/home", function(req, res){
-  res.send("It is really working dude");
+  res.send("It is very very very really working dude");
 });
 
 app.listen(process.env.PORT || 5000);
