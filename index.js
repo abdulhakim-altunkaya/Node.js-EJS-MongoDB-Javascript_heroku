8061b435686aabf1b113662a1b2d32258e739cf4
abdulhakim_altunkaya <altunkaya2@gmail.com>
2021-06-07 19:08:39 +0300

    Commit 11: coding-languages.html created

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 app.get("/test", function(req, res){
   res.sendFile(path.join(__dirname, '/test.html'));
 });
+app.get("/coding-languages", function(req, res){
+  res.sendFile(path.join(__dirname, '/coding-languages.html'));
+});
 app.get("/home", function(req, res){
   res.send("It is very very very really working dude");
 });
