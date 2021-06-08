f9aaf69b6bca83b78db84ff3f811005eda966e6c
abdulhakim_altunkaya <altunkaya2@gmail.com>
2021-06-08 23:16:08 +0300

    Heroku second attempt with new domain

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-
+app.get("/", function(req, res){
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 app.get("/test", function(req, res){
   res.sendFile(path.join(__dirname, '/test.html'));
 });
@@ -12,5 +14,6 @@ app.get("/coding-languages", function(req, res){
 app.get("/home", function(req, res){
   res.send("It is very very very really working dude");
 });
+
 
 app.listen(process.env.PORT || 5000);
