23055b3c1986f4897e6fda2b2b8e9bb803aa8920
abdulhakim_altunkaya <altunkaya2@gmail.com>
2021-06-13 18:57:07 +0300

    arranging website

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const app = express();
 app.use(express.static('assets'))
 
 app.get("/", function(req, res){
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/pages/index.html'));
 });
 app.get("/test", function(req, res){
-  res.sendFile(path.join(__dirname, '/test.html'));
+  res.sendFile(path.join(__dirname, '/pages/test.html'));
 });
 app.get("/coding-skills", function(req, res){
-  res.sendFile(path.join(__dirname, '/coding-languages.html'));
+  res.sendFile(path.join(__dirname, '/pages/coding-languages.html'));
 });
 app.get("/home", function(req, res){
   res.send("It is very very very really working dude");
