fa3572f5b7e3bbfe530b4847df586d3a7a560406
abdulhakim_altunkaya <altunkaya2@gmail.com>
2021-06-14 21:10:00 +0300

    Style separated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static('assets'))
+app.use(express.static('assets'));
 app.set("view engine", "ejs");
 app.use("/assets", express.static("static"));
 
