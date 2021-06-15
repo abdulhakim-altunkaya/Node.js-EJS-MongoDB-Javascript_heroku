fb7025c67a8af738faf5fc6108e4edfe94e8873e
abdulhakim_altunkaya <altunkaya2@gmail.com>
2021-06-15 17:49:54 +0300

    fixing partial template

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.get("/", function(req, res){
   res.sendFile(path.join(__dirname, '/pages/index.html'));
 });
 app.get("/test", function(req, res){
-  res.sendFile(path.join(__dirname, '/pages/test.html'));
+  res.send("test");
 });
 app.get("/coding-skills", function(req, res){
-  res.render("coding");
+  res.render("ejs1");
 });
 app.get("/home", function(req, res){
   res.send("It is very very very really working dude");
