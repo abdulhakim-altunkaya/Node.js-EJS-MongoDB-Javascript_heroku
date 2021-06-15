2085057a57fabe7815f2120b74de317835bda0e1
abdulhakim_altunkaya <altunkaya2@gmail.com>
2021-06-15 20:45:10 +0300

    Website shaping more

--- index.js
+++ index.js
@@ -9,18 +9,31 @@ app.use("/assets", express.static("static"));
 app.get("/", function(req, res){
   res.sendFile(path.join(__dirname, '/pages/index.html'));
 });
-app.get("/test", function(req, res){
-  res.send("test");
-});
 app.get("/coding-skills", function(req, res){
-  res.render("coding");
+  res.render("a1_coding");
 });
-app.get("/home", function(req, res){
-  res.send("It is very very very really working dude");
+app.get("/python-constructor", function(req, res){
+  res.render("a2_python");
+});
+app.get("/deploying-nodejs", function(req, res){
+  res.render("a3_nodejs");
+});
+app.get("/python-pie", function(req, res){
+  res.render("a4_pie");
+});
+app.get("/broken-image-nodejs", function(req, res){
+  res.render("a5_nodejs_image");
 });
 app.get("/ejs1", function(req, res){
   res.render("ejs1");
 });
+app.get("/test", function(req, res){
+  res.send("test");
+});
+app.get("/home", function(req, res){
+  res.send("It is very very very really working dude");
+});
+
 
 
 app.listen(process.env.PORT || 5000);
